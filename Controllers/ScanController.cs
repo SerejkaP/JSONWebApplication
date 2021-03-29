0d2002b5cc7e05e822dc63ca9bd5ced59ef9eaca
SerejkaP <haggardspark@gmail.com>
2021-03-29 12:04:16 +0600

    scan controller

---
+++ Controllers/ScanController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSONWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScanController : ControllerBase
+    {
+        [HttpGet]
+        public string scan()
+        {
+            JsonDataProvider obj = new JsonDataProvider();
+            JsonData jsonData = obj.DeserializeJsonData();
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(jsonData.scan);
+            return json;
+        }
+    }
+}
