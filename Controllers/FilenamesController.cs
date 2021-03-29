3700f5c101bacdb1dae03c7c28833daa99a420cb
SerejkaP <haggardspark@gmail.com>
2021-03-29 16:23:17 +0600

    filenames controller with little flaw

---
+++ Controllers/FilenamesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSONWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilenamesController : ControllerBase
+    {
+        [HttpGet("correct={value}")]
+        public string filenames(bool value)
+        {
+            string json = "";
+            JsonDataProvider obj = new JsonDataProvider();
+            JsonData jsonData = obj.DeserializeJsonData();
+            for( int i = 0; i < jsonData.files.Length; i++)
+            {
+                if (jsonData.files[i].result == value)
+                {
+                    json += Newtonsoft.Json.JsonConvert.SerializeObject(jsonData.files[i].filename);
+                }
+            }
+            return json;
+        }
+    }
+}
