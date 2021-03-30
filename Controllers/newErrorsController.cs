b6a304cde6c06d4dc7386170c0cc7b61010730be
SerejkaP <haggardspark@gmail.com>
2021-03-30 22:11:59 +0600

    non-working newErrors controller

---
+++ Controllers/newErrorsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace JSONWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class newErrorsController : ControllerBase
+    {
+        [HttpPost]
+        public async Task newErrorsAsync([FromBody] string value)
+        {
+            JsonData jsonData = new JsonData();
+            jsonData = JsonConvert.DeserializeObject<JsonData>(value);
+            string path = @"JSONWebApplication\JsonFiles\";
+            if (jsonData.files != null || jsonData.scan != null)
+            {
+                string fullpath = DateTime.Today.ToString();
+                using FileStream fstream = new FileStream($"{path}\\" + fullpath + ".json", FileMode.Create);
+                byte[] array = System.Text.Encoding.Default.GetBytes(value);
+                await fstream.WriteAsync(array, 0, array.Length);
+            }
+        }
+    }
+}
